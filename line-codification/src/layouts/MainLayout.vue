0cc688ed0cc09ddf5227ae753cca78020703f0e2
Lucas <lucas_lima.nog@hotmail.com>
2024-06-24 21:03:00 -0300

    Main Project Files

---
+++ line-codification/src/layouts/MainLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <q-layout view="lHh lpr lFf" container style="height: 400px">
+      <q-header elevated>
+        <q-toolbar>
+          <q-img 
+            src="../assets/icons/byte.png"
+            style="height: 100px; max-width: 100px"
+            class="q-ml-lg q-my-md"
+          />
+
+          <div class="text-h2 text-weight-bold q-ml-md q-my-md">ByteBuster</div>
+        </q-toolbar>
+
+      </q-header>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  align-items: center;
+}
+.q-my-md {
+  margin: 10px 0;
+}
+.q-ml-md {
+  margin-left: 10px;
+}
+</style>
